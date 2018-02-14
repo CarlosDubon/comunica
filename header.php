9ab605fcd8459a2d54d4554676c46767461b42cc
CarlosDubon <carlosminerodubon@gmail.com>
2018-02-14 09:39:33 -0600

    a los inicios

--- header.php
+++ header.php
@@ -12,18 +12,13 @@
     
     <?php wp_head(); ?> 
 </head>
-<body <?php
-        if(!is_home()){
-            echo 'onload="pageimage();"';
-        }
-      ?>>
+<body>
     <div class="logo-container" style="background-image: url('<?php echo get_header_image(); ?>')">
     </div>
     
-    <!-- Barra de dezplazamiento inicial -->
+    <!-- Barra de dezplazamiento inicial 
     <a class="prevent"><h1 class="arrow-down"><span class=" glyphicon glyphicon-chevron-down" aria-hidden="true"></span></h1></a>
-    
-    <!--Barra de barra de navegación principal-->
+    Barra de barra de navegación principal-->
     <nav class="dark-blue-nav" id="first-nav">
         
         <?php
@@ -31,7 +26,7 @@
             'theme_location'=>'secciones',
             'container'=>'div',
             'container_class'=>'section-containers',
-            'items_wrap'=>'<ul class="drak-ul" id="home"> <li class="home-link"><a href="'.home_url().'#home">INICIO</a></li>%3$s</ul>'
+            'items_wrap'=>'<ul class="drak-ul" id="home"> <li class="home-link"><a href="'.home_url().'">INICIO</a></li>%3$s</ul>'
 
         ));
         ?>
