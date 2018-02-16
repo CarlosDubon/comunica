0552638d4fe50a839fff88f185f24b57f25bdaea
Douglas_HdezT <00004516@uca.edu.sv>
2018-02-16 14:55:56 -0600

    Adventure of a lifetime

--- search.php
+++ search.php
@@ -1,5 +1,4 @@
 <?php
-/* Template Name: Search page */
 get_header();
 ?>
 
@@ -12,12 +11,24 @@ get_header();
                     <?php get_search_form(); ?>
                 </div>
                 
-                <?php if(have_posts()){
-                    $result_cont=0; 
-                    while(have_posts()){
-                        the_post();
-                         $result_cont++;
-                    }?>               
+                <?php 
+                
+                    if(have_posts()){
+                        $result_cont=0;
+                        $pages_cont= 0;
+                        while(have_posts()){
+                            the_post();
+                            $post=get_post();
+                            if($post->post_type == 'page'){
+                                $pages_cont++;
+                            }
+                            $result_cont++;
+                        }
+                        
+                    }
+                    if(have_posts() && $pages_cont != $result_cont){
+                        $result_cont = $result_cont - $pages_cont;
+                ?>               
                     
                     <div class="result_panel">
                         <h1>Mostrando resultados para: <span class="inside"><?php the_search_query() ?></span></h1>
@@ -25,23 +36,28 @@ get_header();
                         <div class="result_panel_posts">
                             <?php
                             while(have_posts()){
-                                the_post();?>
+                                the_post();
+                                $post=get_post();
+                                ?>
+                                <?php 
+                                    if($post->post_type == 'post'){?>
+                                        <div class="result_post">
+                                            <div class="row">
+                                                <div class="col-4">
+                                                    <?php
+                                                        if ( has_post_thumbnail() ) {
+                                                        the_post_thumbnail('post-thumbnails',array('class'=>'img-responsive result_image'));
+                                                    }?>
+                                                </div>
+                                                <div class="col-8 result_content">
+                                                       <h1><?php the_title(); ?></h1>
+                                                       <p><?php the_excerpt(); ?></p>
+                                                        <a href="<?php the_permalink();?>" class="btn btn-info">Leer Más <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php $post_printed++; }?>
                                 
-                                <div class="result_post">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <?php
-                                                    if ( has_post_thumbnail() ) {
-                                                    the_post_thumbnail('post-thumbnails',array('class'=>'img-responsive result_image'));
-                                            }?>
-                                        </div>
-                                        <div class="col-8 result_content">
-                                               <h1><?php the_title(); ?></h1>
-                                               <p><?php the_excerpt(); ?></p>
-                                                <a href="<?php the_permalink();?>" class="btn btn-info">Leer Más <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
-                                        </div>
-                                    </div>
-                                </div>
                                 
                             <?php } //END WHILE ?>
                         </div>
